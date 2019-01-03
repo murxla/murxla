07691b01595c3a1f42b7d6146b18ff03b7ce9bb1
Mathias Preiner <mathias.preiner@gmail.com>
2019-01-02 19:03:47 -0800

    Add initial cmake infrastructure + solver submodules.

---
+++ cmake/Helpers.cmake
@@ -0,0 +1,32 @@
+include(CheckCCompilerFlag)
+include(CheckCXXCompilerFlag)
+
+# Add a CXX flag to the global list of CXX flags.
+macro(add_cxx_flag flag)
+  if(CMAKE_CXX_FLAGS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}")
+  else()
+    set(CMAKE_CXX_FLAGS "${flag}")
+  endif()
+  message(STATUS "Configuring with CXX flag '${flag}'")
+endmacro()
+
+# Check if CXX flag is supported and add to global list of CXX flags.
+macro(add_check_cxx_flag flag)
+  string(REGEX REPLACE "[-=]" "_" flagname ${flag})
+  check_cxx_compiler_flag("${flag}" HAVE_FLAG${flagname})
+  if(HAVE_FLAG${flagname})
+    add_cxx_flag(${flag})
+  endif()
+endmacro()
+
+# Add required CXX flag. Configuration fails if the CXX flag is not supported
+# by the compiler.
+macro(add_required_cxx_flag flag)
+  string(REGEX REPLACE "[-=]" "_" flagnamename ${flag})
+  check_cxx_compiler_flag("${flag}" HAVE_FLAG${flagname})
+  if (NOT HAVE_FLAG${flagname})
+    message(FATAL_ERROR "Required compiler flag ${flag} not supported")
+  endif()
+  add_cxx_flag(${flag})
+endmacro()
