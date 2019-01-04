3fd1fe3ee27755d867869f66bf57cb7375f67a9e
Aina Niemetz <aina.niemetz@gmail.com>
2019-01-04 13:35:42 -0800

    Add missing files.

---
+++ src/btor_actions.cpp
@@ -0,0 +1,23 @@
+#ifdef SMTMBT_USE_BOOLECTOR
+
+#include "btor_actions.hpp"
+
+namespace smtmbt {
+namespace btor {
+
+void
+BtorActionNew::run()
+{
+  // TODO
+}
+
+void
+BtorActionDelete::run()
+{
+  // TODO
+}
+
+}  // namespace btor
+}  // namespace smtmbt
+
+#endif
---
+++ src/cvc4_actions.cpp
@@ -0,0 +1,23 @@
+#ifdef SMTMBT_USE_CVC4
+
+#include "cvc4_actions.hpp"
+
+namespace smtmbt {
+namespace cvc4 {
+
+void
+CVC4ActionNew::run()
+{
+  // TODO
+}
+
+void
+CVC4ActionDelete::run()
+{
+  // TODO
+}
+
+}  // namespace cvc4 
+}  // namespace smtmbt
+
+#endif
