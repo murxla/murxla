bbaac0a50b0d199fd0c41de5a51202417b66e744
Aina Niemetz <aina.niemetz@gmail.com>
2020-01-29 16:16:07 -0800

    utils: Add more documentation.

--- src/util.hpp
+++ src/util.hpp
@@ -35,32 +35,43 @@ class RNGenerator
      THIRD,
    };
 
+   /** Constructor. */
    explicit RNGenerator(uint32_t seed = 0);
+   /** Pick uint32_t between 0 and UINT32_MAX. */
    uint32_t pick_uint32();
+   /** Pick uint32_t between 'from' and 'to' (inclusive). */
    uint32_t pick_uint32(uint32_t from, uint32_t to);
+   /** Pick int32_t between 'from' and 'to' (inclusive). */
    int32_t pick_int32(int32_t from, int32_t to);
+   /**
+    * Pick uint32_t between 0 and weights.size(), weighted by the given weights.
+    * The probability to pick each number is w/S with w its weight and S the
+    * sum of all weights.
+    */
    uint32_t pick_uint32_weighted(std::vector<uint32_t>& weights);
+   /** Pick uint64_t between 0 and UINT64_MAX. */
    uint64_t pick_uint64();
+   /** Pick uint64_t between 'from' and 'to' (inclusive). */
    uint64_t pick_uint64(uint64_t from, uint64_t to);
-   /* Pick with given probability, 100% = 1000. */
+   /** Pick with given probability, 100% = 1000. */
    bool pick_with_prob(uint32_t prob);
-   /* Pick with probability of 50%. */
+   /** Pick with probability of 50%. */
    bool flip_coin();
-   /* Pick one out of three choices. */
+   /** Pick one out of three choices. */
    Choice pick_one_of_three();
-   /* Pick random string of given length from the set of 256 printable chars. */
+   /** Pick random string of given length from set of 256 printable chars. */
    std::string pick_string(uint32_t len);
-   /* Pick random string of given length from given character set. */
+   /** Pick random string of given length from given character set. */
    std::string pick_string(std::string& chars, uint32_t len);
-   /* Pick binary string of given length. */
+   /** Pick binary string of given length. */
    std::string pick_bin_string(uint32_t len);
-   /* Pick decimal string of given length in binary representation. */
+   /** Pick decimal string of given length in binary representation. */
    std::string pick_dec_bin_string(uint32_t bin_len);
-   /* Pick hexadecimal string of given length in binary representation. */
+   /** Pick hexadecimal string of given length in binary representation. */
    std::string pick_hex_bin_string(uint32_t bin_len);
-   /* Pick simple symbol string (as defined in SMT-LIB) of given length. */
+   /** Pick simple symbol string (as defined in SMT-LIB) of given length. */
    std::string pick_simple_symbol(uint32_t len);
-   /* Pick piped symbol string (as defined in SMT-LIB) of given length. */
+   /** Pick piped symbol string (as defined in SMT-LIB) of given length. */
    std::string pick_piped_symbol(uint32_t len);
 
    /* Pick random element from given map. */
