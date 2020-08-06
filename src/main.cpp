ada74006f36c2f2f0c31eaf534620239df3118e8
Aina Niemetz <aina.niemetz@gmail.com>
2020-08-06 10:41:30 -0700

    Document Options struct.

--- src/main.cpp
+++ src/main.cpp
@@ -43,25 +43,55 @@ const std::string DEVNULL = "/dev/null";
 
 struct Options
 {
-  uint32_t seed      = 0;
+  /** The current seed. */
+  uint32_t seed = 0;
+  /** The verbosity level. */
   uint32_t verbosity = 0;
-  double time        = 0;
-  uint32_t max_runs  = 0;
+  /** The time limit for one test run (one API sequence). */
+  double time = 0;
+  /** The maximum number of test runs to perform. */
+  uint32_t max_runs = 0;
 
-  bool is_seeded      = false;
+  /** True if seed is provided by user. */
+  bool is_seeded = false;
+  /** True to include state of RNG in every step of the API trace. */
   bool trace_seeds = false;
+  /** True to use simple instead of completely random symbols for inputs. */
   bool simple_symbols = false;
-  bool smt         = false;
+  /** True to only generate SMT-LIB compliant API traces. */
+  bool smt = false;
+  /** True to print statistics. */
   bool print_stats = false;
+
+  /** The selected solver to test. */
   std::string solver;
+  /** The path to the solver binary to test when --smt2 is enabled. */
   std::string solver_binary;
+  /** The file to trace the API call sequence to. */
   std::string api_trace_file_name;
+  /** The API trace file to replay. */
   std::string untrace_file_name;
+  /** The file to dump the SMT-LIB2 representation of the current trace to. */
   std::string smt2_file_name;
+
+  /**
+   * True if the API trace of the current run should be reduced by means of
+   * delta-debugging.
+   * If seeded or when untracing, current trace will be reduced no matter if
+   * it triggers an error or not. In continuous mode, only error inducing
+   * traces are reduced.
+   */
   bool dd = false;
+  /** The file to write the reduced API trace to. */
   std::string dd_trace_file_name;
+
+  /* The name of the solver to cross-check given solver with. */
   std::string cross_check;
+
+  /* The name of the options file of the enabled solver. */
   std::string solver_options_file;
+
+  /* The list of currently enabled theories. */
   TheoryIdVector enabled_theories;
 };
 
@@ -415,7 +445,8 @@ parse_options(Options& options, int argc, char* argv[])
       if (!options.cross_check.empty())
       {
         std::stringstream es;
-        es << "option " << arg << " is incompatible with option -c, --cross-check";
+        es << "option " << arg
+           << " is incompatible with option -c, --cross-check";
         die(es.str());
       }
       options.solver = SMTMBT_SOLVER_SMT2;
